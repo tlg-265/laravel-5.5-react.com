10faffc8d385882c885a862825a3d04663eb8067
Web soluction <51754234+will0225@users.noreply.github.com>
2020-02-18 11:11:10 +0800

    Update listener.js
    finished

--- template/listener.js
+++ template/listener.js
@@ -3,10 +3,15 @@
  * While: "$ npm run watch" is running,
  * whenever some of the files: /template/template-XX.json is modified,
  * the current script should be executed.
- * To the variable: "template" should be assigned the name of that file,
+ * To the variable: "tmplate" should be assigned the name of that file,
  * for example: 'template-02.json'
  */
 
 var template = '###TBD###';
+var watch = require('node-watch');
+watch('./', { recursive: true }, function(evt, name) {
+  console.log('%s changed.', name);
+});
 
-console.log(`The template: ${template} was modified.`);
+// console.log((template1['text-color']));
+// console.log(`The template: ${template} was modified.`);
